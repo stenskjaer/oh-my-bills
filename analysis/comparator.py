afd30ba0bb72ea8eddcc5726811c3313dc7eebe5
Michael Stenskjær Christensen <mschristensen@computas.com>
2020-11-22 11:30:44 +0100

    Make methods internal

--- analysis/comparator.py
+++ analysis/comparator.py
@@ -19,35 +19,35 @@ class TrigramSimilarity:
     def compare(
         self, present: Comparable, others: List[Comparable]
     ) -> List[Comparable]:
-        scores = pd.Series(self.run_comparison(present, others))
+        scores = pd.Series(self._run_comparison(present, others))
         dev = scores.std()
         most_similar = scores[scores > dev]
         return most_similar.index
 
-    def run_comparison(
+    def _run_comparison(
         self, present: Comparable, others: List[Comparable]
     ) -> Dict[Comparable, float]:
         res = {}
         for other in others:
-            quotient = self.similarity(present.comparandum(), other.comparandum())
+            quotient = self._similarity(present.comparandum(), other.comparandum())
             res[other] = quotient
         return res
 
-    def similarity(self, text1: str, text2: str, number: int = 3) -> float:
+    def _similarity(self, text1: str, text2: str, number: int = 3) -> float:
         """
         Finds the similarity between 2 strings using ngrams.
         0 being completely different strings, and 1 being equal strings
         """
 
-        ngrams1 = self.find_ngrams(text1, number)
-        ngrams2 = self.find_ngrams(text2, number)
+        ngrams1 = self._find_ngrams(text1, number)
+        ngrams2 = self._find_ngrams(text2, number)
 
         num_unique = len(ngrams1 | ngrams2)
         num_equal = len(ngrams1 & ngrams2)
         return float(num_equal) / float(num_unique)
 
     @staticmethod
-    def find_ngrams(text: str, size: int = 3) -> set:
+    def _find_ngrams(text: str, size: int = 3) -> set:
         ngrams: Set[str] = set()
         if not text:
             return ngrams
