cfc4be37cfc0aa0f789cdcd92ba1e950ec849b30
Michael Stenskjær Christensen <mschristensen@computas.com>
2020-11-23 12:42:28 +0100

    Add missing eq check on transaction

--- receiver/transactions.py
+++ receiver/transactions.py
@@ -15,7 +15,8 @@ class Datable(ABC):
 
 
 class Transaction(Comparable, Datable):
-    """Transaction represents a single transaction from a bank statement."""
+    """Transaction represents a single transaction from a bank statement. They
+    contain the date of the transaction, a description, and the amount transferred."""
 
     def __init__(
         self,
@@ -29,7 +30,17 @@ class Transaction(Comparable, Datable):
         self.normalized_description = self.normalize(description)
 
     def __repr__(self) -> str:
-        return f"<Transaction: '{self.normalized_description}'>"
+        return (
+            f"<Transaction: description='{self.normalized_description}', "
+            f"date={self.date()}, amount={self.amount}>"
+        )
+
+    def __eq__(self, other) -> bool:
+        return (self.description, self.date(), self.amount) == (
+            other.description,
+            other.date(),
+            other.amount,
+        )
 
     def comparandum(self) -> str:
         """Returns a representation of the transaction for identifying similar
--- test/transactions_test.py
+++ test/transactions_test.py
@@ -5,14 +5,16 @@ from receiver.transactions import Transaction
 
 class TestTransaction:
     date = parser.parse("03-02-2020", dayfirst=True)
-    sut = Transaction("description", date)
+    amount = 10.0
+    desc = "description"
+    sut = Transaction(desc, date, amount)
 
     def test_normalization(self):
         expect = "description"
         assert self.sut.normalized_description == expect
 
     def test_repr(self):
-        expect = "<Transaction: 'description'>"
+        expect = "<Transaction: description='description', date=2020-02-03 00:00:00, amount=10.0>"
         assert self.sut.__repr__() == expect
 
     def test_comparandum(self):
@@ -23,3 +25,7 @@ class TestTransaction:
         expect = self.date
         actual = self.sut.date()
         assert actual == expect
+
+    def test_eq(self):
+        other = Transaction(self.desc, self.date, self.amount)
+        assert self.sut.__eq__(other)
