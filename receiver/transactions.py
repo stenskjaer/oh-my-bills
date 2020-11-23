e683d090253785be5db1ab202d0343396e223b9a
Michael Stenskjær Christensen <mschristensen@computas.com>
2020-11-23 14:13:52 +0100

    Add normalization to transaction records

--- receiver/transactions.py
+++ receiver/transactions.py
@@ -18,6 +18,16 @@ class Transaction(Comparable, Datable):
     """Transaction represents a single transaction from a bank statement. They
     contain the date of the transaction, a description, and the amount transferred."""
 
+    rules = [
+        lambda x: x.lower(),
+        lambda x: x.replace("visa/dankort", ""),
+        lambda x: x.replace("dankort", ""),
+        lambda x: x.replace("visa/mastercard", ""),
+        lambda x: x.replace("mastercard", ""),
+        lambda x: x.replace("mobilepay", ""),
+        lambda x: " " + x.strip() + " ",
+    ]
+
     def __init__(
         self,
         description: str,
@@ -48,11 +58,12 @@ class Transaction(Comparable, Datable):
     def comparandum(self) -> str:
         """Returns a representation of the transaction for identifying similar
         transactions."""
-        return self.description
+        return self.normalized_description
 
     def date(self) -> datetime:
         return self.datetime
 
-    @staticmethod
-    def normalize(description: str) -> str:
-        return description
+    def normalize(self, value: str) -> str:
+        for rule in self.rules:
+            value = rule(value)
+        return value
